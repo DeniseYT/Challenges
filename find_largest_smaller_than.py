fe5dc2c0892d678a16ee7b9f7d19bf41bb4044ba
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 05:36:55 +0000

    Initial commit

---
+++ find_largest_smaller_than.py
@@ -0,0 +1,19 @@
+Given an ordered list of numbers and a number, return the index of the largest number in the list that is smaller than that number.
+
+For example:
+>>> find_largest_smaller_than([-5, -2, 8, 12, 32], 10)
+2
+
+>>> find_largest_smaller_than([-5, -2, 8, 12, 32], 33)
+4
+
+>>> find_largest_smaller_than([-5, -2, 8, 12, 32], -1)
+1
+
+Never find xnumber — it’s not smaller than itself!
+>>> find_largest_smaller_than([-5, -2, 8, 12, 32], 8)
+1
+
+If no such number exists, return None:
+>>> find_largest_smaller_than([-5, -2, 8, 12, 32], -10) is None
+True
