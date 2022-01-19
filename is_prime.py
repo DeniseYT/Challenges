1ef66a972bf5053b5a220aefebc7bbaec07857f5
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 05:27:32 +0000

    My solution 1

--- is_prime.py
+++ is_prime.py
@@ -1,24 +1,41 @@
-Write a function the returns True or False, depending on whether the integer passed into it is a prime number.
+# Write a function the returns True or False, depending on whether the integer passed into it is a prime number.
 
-Only numbers >1 can be prime numbers:
->>> is_prime(0)
-False
+# Only numbers >1 can be prime numbers:
+# >>> is_prime(0)
+# False
 
->>> is_prime(1)
-False
+# >>> is_prime(1)
+# False
 
-Any number >1 that has no divisors other than 1 and itself is a prime number:
->>> is_prime(2)
-True
+# Any number >1 that has no divisors other than 1 and itself is a prime number:
+# >>> is_prime(2)
+# True
 
->>> is_prime(3)
-True
+# >>> is_prime(3)
+# True
 
->>> is_prime(4)
-False
+# >>> is_prime(4)
+# False
 
->>> is_prime(11)
-True
+# >>> is_prime(11)
+# True
 
->>> is_prime(999)
-False
+# >>> is_prime(999)
+# False
+
+
+# Start my solution:
+
+# My solution 1
+def is_prime(num):
+
+    if num <= 1:
+        return False
+    
+    elif num % 2 == 0 or num % 3 == 0 or num % 5 == 0 or num % 7 == 0:
+        return False
+    
+    else:
+        return True
+
+print(is_prime(4))
