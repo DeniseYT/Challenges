5a53de824d361b680cb3cdb0f1609592749b2023
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 04:21:34 +0000

    Start coding

--- add_to_zero.py
+++ add_to_zero.py
@@ -1,23 +1,33 @@
-Given list of ints, return True if any two nums in list sum to 0.
+# Given list of ints, return True if any two nums in list sum to 0.
 
-Examples
->>> add_to_zero([])
-False
+# Examples
+# >>> add_to_zero([])
+# False
 
->>> add_to_zero([1])
-False
+# >>> add_to_zero([1])
+# False
 
->>> add_to_zero([1, 2, 3])
-False
+# >>> add_to_zero([1, 2, 3])
+# False
 
->>> add_to_zero([1, 2, 3, -2])
-True
+# >>> add_to_zero([1, 2, 3, -2])
+# True
 
-Given the wording of our problem, a zero in the list will always make this true, since “any two numbers” could include that same zero for both numbers, and they sum to zero:
+# Given the wording of our problem, a zero in the list will always make this true, since “any two numbers” could include that same zero for both numbers, and they sum to zero:
 
-Examples
->>> add_to_zero([0, 1, 2])
-True
+# Examples
+# >>> add_to_zero([0, 1, 2])
+# True
 
 
+def add_to_zero(nums):
+    """Given list of ints, return True if any two nums sum to 0."""
 
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            if nums[i] + nums[j] == 0 or 0 in nums:
+                return True
+    else:
+        return False
+
+print(add_to_zero([1, 2, 3, -2]))
