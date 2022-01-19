aeda12237dbcf00773d2d58a73891026d6668f53
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 05:13:46 +0000

    Initial commit

---
+++ has_unique_chars.py
@@ -0,0 +1,15 @@
+Given a word, return True if that word contains only unique characters. Return False otherwise.
+
+>>> has_unique_chars("Monday")
+True
+
+>>> has_unique_chars("Moonday")
+False
+
+>>> has_unique_chars("")
+True
+
+Uppercase and lowercase letters should be considered separately:
+
+>>> has_unique_chars("Bob")
+True
