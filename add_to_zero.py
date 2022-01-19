56950bc2f08e0dcc0dbc68cfb79423c4559fc77e
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 04:27:02 +0000

    Option 2

--- add_to_zero.py
+++ add_to_zero.py
@@ -20,6 +20,9 @@
 # True
 
 
+# Start my solution:
+
+# Option 1
 def add_to_zero(nums):
     """Given list of ints, return True if any two nums sum to 0."""
 
@@ -31,3 +34,22 @@ def add_to_zero(nums):
         return False
 
 print(add_to_zero([1, 2, 3, -2]))
+
+
+# Option 2
+def add_to_zero(nums):
+    
+    new_nums = set(nums)
+
+    for num in new_nums:
+        if -num in new_nums:
+            return True
+        # else:
+        #     return False
+
+    return False
+
+print(add_to_zero([1, 2, 3, -2]))
+
+
+
