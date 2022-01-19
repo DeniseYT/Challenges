96e327641bc2d0ff18e22be736d62fac6507b6b3
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 05:47:54 +0000

    Initial commit

---
+++ translate_leet.py
@@ -0,0 +1,18 @@
+in which standard letters are often replaced by numerals or special characters.
+
+Letter -> Becomes
+
+a -> @
+o -> 0
+e -> 3
+l -> 1
+s -> 5
+t -> 7
+
+In this exercise, you’ll make a function that translate a word to leet-speak:
+
+>>> translate_leet("Hi Balloonicorn")
+'Hi B@1100nic0rn'
+
+>>> translate_leet("Hackbright is the Shizzle")
+'H@ckbrigh7 i5 7h3 5hizz13'
