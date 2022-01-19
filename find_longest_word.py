ca0a8f1a6527f85014c8535a246219cabbf4d592
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 06:15:21 +0000

    Initial commit

---
+++ find_longest_word.py
@@ -0,0 +1,9 @@
+Write a function, find_longest_word, that takes a list of words and returns the length of the longest one.
+
+# Examples
+>>> find_longest_word(["hi", "hello"])
+5
+
+>>> find_longest_word(["Balloonicorn", "Hackbright"])
+12
+
