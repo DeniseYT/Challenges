ffd3459db4359904288b185023b1ef9c7090d1e5
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 04:36:16 +0000

    Option 1

--- concat_lists.py
+++ concat_lists.py
@@ -1,18 +1,36 @@
-Given two lists. concatenate them (that is, combine them into a single list).
-For example, given [1, 2] and [3, 4]:
+# Given two lists. concatenate them (that is, combine them into a single list).
+# For example, given [1, 2] and [3, 4]:
 
-# Examples
->>> concat_lists([1, 2], [3, 4])
-[1, 2, 3, 4]
+# # Examples
+# >>> concat_lists([1, 2], [3, 4])
+# [1, 2, 3, 4]
 
-It should work if either list is empty:
+# It should work if either list is empty:
 
-# Examples
->>> concat_lists([], [1, 2])
-[1, 2]
+# # Examples
+# >>> concat_lists([], [1, 2])
+# [1, 2]
 
->>> concat_lists([1, 2], [])
-[1, 2]
+# >>> concat_lists([1, 2], [])
+# [1, 2]
 
->>> concat_lists([], [])
-[]
+# >>> concat_lists([], [])
+# []
+
+
+# Start my solution:
+
+# Option 1
+def concat_lists(list1, list2):
+
+    new_list = []
+
+    for item1 in list1:
+        new_list.append(item1)
+    
+    for item2 in list2:
+        new_list.append(item2)
+
+    return new_list
+
+print(concat_lists([], [1, 2]))
