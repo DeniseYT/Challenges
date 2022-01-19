8eacc0901e84d75f7d86cd8fa2e821497ef9c3cf
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 05:49:24 +0000

    My solution

--- translate_leet.py
+++ translate_leet.py
@@ -1,18 +1,67 @@
-in which standard letters are often replaced by numerals or special characters.
+# in which standard letters are often replaced by numerals or special characters.
 
-Letter -> Becomes
+# Letter -> Becomes
 
-a -> @
-o -> 0
-e -> 3
-l -> 1
-s -> 5
-t -> 7
+# a -> @
+# o -> 0
+# e -> 3
+# l -> 1
+# s -> 5
+# t -> 7
 
-In this exercise, you’ll make a function that translate a word to leet-speak:
+# In this exercise, you’ll make a function that translate a word to leet-speak:
 
->>> translate_leet("Hi Balloonicorn")
-'Hi B@1100nic0rn'
+# >>> translate_leet("Hi Balloonicorn")
+# 'Hi B@1100nic0rn'
 
->>> translate_leet("Hackbright is the Shizzle")
-'H@ckbrigh7 i5 7h3 5hizz13'
+# >>> translate_leet("Hackbright is the Shizzle")
+# 'H@ckbrigh7 i5 7h3 5hizz13'
+
+
+# Start my solution:
+
+# My solution 
+def translate_leet(phrase):
+
+    leet_speak = {
+        "a": "@",
+        "o": "0",
+        "e": "3",
+        "l": "1",
+        "s": "5",
+        "t": "7",
+    }
+    
+    new_phrase = ""
+
+    for letter in phrase:
+        if letter == "a":
+            letter = leet_speak["a"]
+            new_phrase += letter
+
+        elif letter == "o":
+            letter = leet_speak["o"]
+            new_phrase += letter
+
+        elif letter == "e":
+            letter = leet_speak["e"]
+            new_phrase += letter
+            
+        elif letter == "l":
+            letter = leet_speak["l"]
+            new_phrase += letter
+            
+        elif letter == "s":
+            letter = leet_speak["s"]
+            new_phrase += letter
+            
+        elif letter == "t":
+            letter = leet_speak["t"]
+            new_phrase += letter
+        
+        else:
+            new_phrase += letter
+            
+    return new_phrase
+
+print(translate_leet("Hi Balloonicorn"))
