6856a31285053273fff5ac0eedb825ec68b9baa1
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 06:24:20 +0000

    My solution 2

--- max_of_three.py
+++ max_of_three.py
@@ -23,3 +23,18 @@ def max_of_three(num1, num2, num3):
     return largest
 
 print(max_of_three(1, 5, 2))
+
+
+# My solution 2
+def max_of_three(num1, num2, num3):
+
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    elif num3 >= num1 and num3 >= num2:
+        return num3
+    else:
+        return "no largest"
+
+print(max_of_three(5, 5, 5))
