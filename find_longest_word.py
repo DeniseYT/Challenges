02c162dfa220a57bef1cd0bcfdb9d4346da9c1c5
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 06:16:58 +0000

    My solution

--- find_longest_word.py
+++ find_longest_word.py
@@ -1,9 +1,27 @@
-Write a function, find_longest_word, that takes a list of words and returns the length of the longest one.
+# Write a function, find_longest_word, that takes a list of words and returns the length of the longest one.
 
-# Examples
->>> find_longest_word(["hi", "hello"])
-5
+# # Examples
+# >>> find_longest_word(["hi", "hello"])
+# 5
 
->>> find_longest_word(["Balloonicorn", "Hackbright"])
-12
+# >>> find_longest_word(["Balloonicorn", "Hackbright"])
+# 12
 
+
+# Start my solution:
+
+# My solution
+def find_longest_word(words):
+
+    largest = len(words[0])
+
+    for word in words:
+        if len(word) > largest:
+            largest = len(word)
+    
+    return largest
+
+    # list comprehension
+    # return max([len(word) for word in words])
+
+print(find_longest_word(["Balloonicorn", "Hackbright"]))
