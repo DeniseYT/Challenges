0da62d22eba81a89121704849e4f4a09f72c73ff
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 06:19:46 +0000

    My solution

--- max_num.py
+++ max_num.py
@@ -1,6 +1,21 @@
-Find the largest integer in a list of integers.
+# Find the largest integer in a list of integers.
 
-For example:
->>> max_num([5, 3, 6, 2, 1])
-6
+# For example:
+# >>> max_num([5, 3, 6, 2, 1])
+# 6
 
+
+# Start my solution:
+
+# My solution 
+def max_num(num_list):
+
+    largest = num_list[0]
+
+    for num in num_list:
+        if num > largest:
+            largest = num
+    
+    return largest
+
+print(max_num([5, 3, 6, 2, 10]))
