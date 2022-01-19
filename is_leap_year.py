114e4ed96989d4e0ae87d92a46499c9b442bdc30
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 04:45:42 +0000

    Initial commit

---
+++ is_leap_year.py
@@ -0,0 +1,8 @@
+Given a string with a month and a year (separated by a space), return the number of days in that month.
+Leap years are a bit tricky. A year is a leap year if and only if:
+it is evenly divisible by 4
+except if it is divisible by 100, in which case it isn’t
+except if it is divisible by 400, in which case it is
+So, for example, 1904 was a leap year. 1900 is divisible by 100, so it wasn’t. 2000 is divisible by 400, so it was.
+
+
