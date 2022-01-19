fe9f01bc28c70f914b08d8437ae1a4917428c1fc
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 05:08:21 +0000

    Initial commit

---
+++ has_more_vowels.py
@@ -0,0 +1,23 @@
+Given a word in English, return True if that word contains more vowels than non-vowels; 
+otherwise, return False. 
+The word will always be a single word, without any punctuation or spaces. It will contain only uppercase and lowercase letters.
+
+If the phrase is over half vowels, it should return True:
+>>> has_more_vowels("moose")
+True
+
+If it’s half vowels (or less), it’s false:
+>>> has_more_vowels("mice")
+False
+
+>>> has_more_vowels("graph")
+False
+
+Don’t consider “y” as a vowel:
+>>> has_more_vowels("yay")
+False
+
+Uppercase vowels are still vowels:
+>>> has_more_vowels("Aal")
+True
+
