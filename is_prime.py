17de6f7ba98c276d2f8c28f55b811ff60214298e
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 05:28:37 +0000

    My solution 2

--- is_prime.py
+++ is_prime.py
@@ -39,3 +39,18 @@ def is_prime(num):
         return True
 
 print(is_prime(4))
+
+
+# My solution 2
+def is_prime(num):
+
+    if num < 2:
+        return False
+    
+    for n in range(2, num):
+        if num % n == 0:
+            return False
+    
+    return True
+
+print(is_prime(999))
