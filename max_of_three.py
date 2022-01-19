6179ca81d7e93321f6aa947784d004a7ea0d94fc
DeniseYT <denise.ytlin@gmail.com>
2022-01-19 06:23:11 +0000

    My solution 1

--- max_of_three.py
+++ max_of_three.py
@@ -1,8 +1,25 @@
-Define a function max_of_three that takes in three numbers as arguments and returns the largest of them.
+# Define a function max_of_three that takes in three numbers as arguments and returns the largest of them.
 
-For example:
->>> maxofthree(1, 5, 2)
-5
+# For example:
+# >>> maxofthree(1, 5, 2)
+# 5
 
->>> maxofthree(10, 1, 11)
-11
+# >>> maxofthree(10, 1, 11)
+# 11
+
+
+# Start my solution:
+
+# My solution 1
+def max_of_three(num1, num2, num3):
+
+    largest = num1
+    nums = (num1, num2, num3)
+
+    for num in nums:
+        if num > largest:
+            largest = num
+    
+    return largest
+
+print(max_of_three(1, 5, 2))
