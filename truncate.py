1d559ae063bffcf742470d013bc71a0e88b6b819
DeniseYT <denise.ytlin@gmail.com>
2022-01-20 07:49:11 +0000

    Initial commit

---
+++ truncate.py
@@ -0,0 +1,11 @@
+Write a function that takes in a string and returns a string where any repeating characters have been truncated to one character. For example:
+
+>>> truncate('aaaaabbbbbcccaaaa')
+'abca'
+
+>>> truncate('hi there')
+'hi there'
+
+>>> truncate('hi   !!! wooow')
+'hi ! wow'
+
