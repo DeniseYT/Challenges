d5eacd008bec9406731bed1e7f0976dbd1ed01fb
DeniseYT <denise.ytlin@gmail.com>
2022-01-20 07:35:39 +0000

    Initial commit

---
+++ snake_to_camel.py
@@ -0,0 +1,8 @@
+Python programmers write variable names in snake_case, where each word is lowercased and joined by underscores. JavaScript programmers write variable names in camelCase, where the initial word is lowercase, and other words are capitalized and jammed together.
+
+Given a variable name in snake_case, return a string with that variable name written in camelCase.
+
+For example:
+>>> snake_to_camel("hi_balloonicorn")
+'hiBalloonicorn'
+
