78d29070b07e515828b3c030def4d94ba48cd768
DeniseYT <denise.ytlin@gmail.com>
2022-01-20 07:09:17 +0000

    My solution 1

--- replace_vowels.py
+++ replace_vowels.py
@@ -1,24 +1,44 @@
-Given list of chars, return a new copy, but with vowels replaced by *.
+# Given list of chars, return a new copy, but with vowels replaced by *.
 
-For example:
->>> replace_vowels(['h', 'i'])
-['h', '*']
+# For example:
+# >>> replace_vowels(['h', 'i'])
+# ['h', '*']
 
->>> replace_vowels(['o', 'o', 'o'])
-['*', '*', '*']
+# >>> replace_vowels(['o', 'o', 'o'])
+# ['*', '*', '*']
 
->>> replace_vowels(['z', 'z', 'z'])
-['z', 'z', 'z']
+# >>> replace_vowels(['z', 'z', 'z'])
+# ['z', 'z', 'z']
 
-An empty list should return an empty list:
->>> replace_vowels([])
-[]
+# An empty list should return an empty list:
+# >>> replace_vowels([])
+# []
 
-Make sure to handle uppercase:
->>> replace_vowels(["A", "b"])
-['*', 'b']
+# Make sure to handle uppercase:
+# >>> replace_vowels(["A", "b"])
+# ['*', 'b']
 
-Do not consider y a vowel:
->>> replace_vowels(["y", "a", "y"])
-['y', '*', 'y']
+# Do not consider y a vowel:
+# >>> replace_vowels(["y", "a", "y"])
+# ['y', '*', 'y']
+
+
+# Start my solution:
+
+# My solution 1
+def replace_vowels(chars):
+
+    vowels = {"a", "e", "i", "o", "u"}
+    new_list = []
+
+    for char in chars:
+        if char.lower() not in vowels:
+            new_list.append(char)
+        
+        else:
+            new_list.append("*")
+            
+    return new_list
+    
+print(replace_vowels(["A", "b"]))
 
