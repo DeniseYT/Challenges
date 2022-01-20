cd7eb2f5e5fde8cd946a6dfa5097bf83b9881dac
DeniseYT <denise.ytlin@gmail.com>
2022-01-20 07:47:13 +0000

    My solution 1

--- sum_list.py
+++ sum_list.py
@@ -1,6 +1,20 @@
-Compute the sum of a list of numbers.
+# Compute the sum of a list of numbers.
 
-For example:
->>> sum_list([5, 3, 6, 2, 1])
-17
+# For example:
+# >>> sum_list([5, 3, 6, 2, 1])
+# 17
 
+
+# Start my solution:
+
+# My solution 
+def sum_list(num_list):
+
+    total = 0
+
+    for num in num_list:
+        total += num
+    
+    return total
+
+print(sum_list([5, 3, 6, 2, 1]))
