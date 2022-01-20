215804be094dc473223bd442d06d1b6ecdfdcfc1
DeniseYT <denise.ytlin@gmail.com>
2022-01-20 07:26:47 +0000

    My solution 2

--- rev_string.py
+++ rev_string.py
@@ -7,7 +7,7 @@
 
 # Start my solution:
 
-# My solution 1
+# My solution 1 - without using python built-in functions reverse or reversed
 def rev_string(astring):
 
     # astring[0] = astring[-1]
@@ -24,3 +24,18 @@ def rev_string(astring):
 
 print(rev_string("porcupine"))
 
+
+# My solution 2 - without using python built-in functions reverse or reversed
+def rev_string(astring):
+
+    new_string = list(astring) # string to list
+    # print(new_string) #['p', 'o', 'r', 'c', 'u', 'p', 'i', 'n', 'e']
+
+    for i in range(len(new_string) // 2):
+        # x, y = y, x
+        new_string[i], new_string[-i-1] = new_string[-i-1],new_string[i]
+
+    # print(new_string) #['e', 'n', 'i', 'p', 'u', 'c', 'r', 'o', 'p']
+    return "".join(new_string) # list to string
+
+print(rev_string("porcupine"))
