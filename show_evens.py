ec53b67d866123a98401f201f1da6901c6323d4a
DeniseYT <denise.ytlin@gmail.com>
2022-01-20 07:30:35 +0000

    My solution 1

--- show_evens.py
+++ show_evens.py
@@ -1,9 +1,23 @@
-You will be given a list of integers, some even and some odd:
->>> lst = [1, 2, 3, 4, 6, 8]
+# You will be given a list of integers, some even and some odd:
+# >>> lst = [1, 2, 3, 4, 6, 8]
 
-Write a function that returns the indices (0-based, as usual in Python) of all the numbers which are even.
-So, for our list above, that would be:
->>> show_evens(lst)
-[1, 3, 4, 5]
+# Write a function that returns the indices (0-based, as usual in Python) of all the numbers which are even.
+# So, for our list above, that would be:
+# >>> show_evens(lst)
+# [1, 3, 4, 5]
 
 
+# Start my solution:
+
+# My solution 1
+def show_evens(nums):
+
+    new_list = []
+
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_list.append(i)
+    
+    return new_list
+
+print(show_evens([1, 2, 3, 4, 6, 8]))
