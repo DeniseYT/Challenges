d28bb419a7430a62ed444c1262862b665c6b38aa
DeniseYT <denise.ytlin@gmail.com>
2022-01-20 07:28:51 +0000

    Initial commit

---
+++ show_evens.py
@@ -0,0 +1,9 @@
+You will be given a list of integers, some even and some odd:
+>>> lst = [1, 2, 3, 4, 6, 8]
+
+Write a function that returns the indices (0-based, as usual in Python) of all the numbers which are even.
+So, for our list above, that would be:
+>>> show_evens(lst)
+[1, 3, 4, 5]
+
+
