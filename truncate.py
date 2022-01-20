359d361182eadae2a624aca8187c664d8994d8c1
DeniseYT <denise.ytlin@gmail.com>
2022-01-20 07:53:32 +0000

    My solution

--- truncate.py
+++ truncate.py
@@ -1,11 +1,27 @@
-Write a function that takes in a string and returns a string where any repeating characters have been truncated to one character. For example:
+# Write a function that takes in a string and returns a string where any repeating characters have been truncated to one character. For example:
 
->>> truncate('aaaaabbbbbcccaaaa')
-'abca'
+# >>> truncate('aaaaabbbbbcccaaaa')
+# 'abca'
 
->>> truncate('hi there')
-'hi there'
+# >>> truncate('hi there')
+# 'hi there'
 
->>> truncate('hi   !!! wooow')
-'hi ! wow'
+# >>> truncate('hi   !!! wooow')
+# 'hi ! wow'
 
+
+# Start my solution:
+
+# My solution 
+def truncate(astring):
+
+    new_string = []
+
+    for letter in astring:
+        if letter not in new_string or letter != new_string[-1]:
+            new_string.append(letter)
+    
+    # print(new_string) #['h', 'i', ' ', '!', ' ', 'w', 'o', 'w']
+    return "".join(new_string) #convert list to string
+
+print(truncate('hi   !!! wooow'))
