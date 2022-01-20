54e15b74ac4e2c5d4eab38e05c49a6f9966c5d49
DeniseYT <denise.ytlin@gmail.com>
2022-01-20 07:13:36 +0000

    Initial commit

---
+++ rev_list_in_place.py
@@ -0,0 +1,8 @@
+Reverse a list in-place. Remember, for this to be “in-place” it can only use a small, constant amount of extra storage space, so no duplicating the list!
+
+For example:
+>>> lst = [1, 2, 3]
+>>> rev_list_in_place(lst)
+>>> lst
+[3, 2, 1]
+
