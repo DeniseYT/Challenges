c1d27e816beeaa88877097df21436834cd4b7021
DeniseYT <denise.ytlin@gmail.com>
2022-01-25 06:57:16 +0000

    Update:remove dict

--- find_largest_even.py
+++ find_largest_even.py
@@ -13,29 +13,15 @@ def find_largest_even(lst):
     """Find largest even number in a list"""
 
     new_lst = lst.split() 
-    print(new_lst) #['what', 'I', 'love', 'is', 'technical', 'challenge']
+    # print(new_lst) #['what', 'I', 'love', 'is', 'technical', 'challenge']
 
-    new_dict = {} 
-    even_lst = []
+    largest_length = 0
 
     for word in new_lst:
-        if len(word) not in new_dict:
-            new_dict[word] = len(word) #{'what': 4, 'I': 1, 'love': 4, 'is': 2, 'technical': 9, 'challenge': 9}
+        if len(word) > largest_length:
+            largest_length = len(word)
     
-    for word, value in new_dict.items():
-        if value % 2 == 0:
-            even_lst.append(word)
-    
-    # print(even_lst) #['what', 'love', 'is']
-
-    largest = even_lst[0]
-    # print(largest) #what
-
-    for word in even_lst:
-        if len(word) > len(largest):
-            largest = len(word)
-    
-    return largest
+    return word
 
 print(find_largest_even("what I love is technical challenge"))
 
