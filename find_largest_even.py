959c55cfd09004afbebc7c0b90b6819e80526a30
DeniseYT <denise.ytlin@gmail.com>
2022-01-25 04:02:46 +0000

    update my solution

--- find_largest_even.py
+++ find_largest_even.py
@@ -1,22 +1,43 @@
 # Atlassin
-# find the largest even number
+# find the largest even number word
 
-# input = [2,2,1,8,3,5]
-# output = 8
+# input = "what I love is technical challenge"
+# output = [what]
+
+# explanation
+# input_lenth = [4,1,4,2,9,9]
+# return the first word of even number
 
 
 def find_largest_even(lst):
     """Find largest even number in a list"""
 
-    new_lst = []
+    new_lst = lst.split() 
+    print(new_lst) #['what', 'I', 'love', 'is', 'technical', 'challenge']
 
-    for num in lst:
-        if num % 2 == 0:
-            new_lst.append(num)
+    new_dict = {} 
+    even_lst = []
+
+    for word in new_lst:
+        if len(word) not in new_dict:
+            new_dict[word] = len(word) #{'what': 4, 'I': 1, 'love': 4, 'is': 2, 'technical': 9, 'challenge': 9}
     
-    return max(new_lst)
+    for word, value in new_dict.items():
+        if value % 2 == 0:
+            even_lst.append(word)
+    
+    # print(even_lst) #['what', 'love', 'is']
 
-print(find_largest_even([2,2,1,8,3,5]))
+    largest = even_lst[0]
+    # print(largest) #what
+
+    for word in even_lst:
+        if len(word) > len(largest):
+            largest = len(word)
+    
+    return largest
+
+print(find_largest_even("what I love is technical challenge"))
 
 
 
