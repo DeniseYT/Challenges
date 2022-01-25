5fd425d1f1b7be81a593cf05a8e010dd2d59ea1d
DeniseYT <denise.ytlin@gmail.com>
2022-01-25 01:46:12 +0000

    My solution

--- find_largest_even.py
+++ find_largest_even.py
@@ -1,11 +1,22 @@
 # Atlassin
-find the largest even number
+# find the largest even number
 
-input = [2,2,1,8,3,5]
-output = 8
+# input = [2,2,1,8,3,5]
+# output = 8
 
 
+def find_largest_even(lst):
+    """Find largest even number in a list"""
 
+    new_lst = []
+
+    for num in lst:
+        if num % 2 == 0:
+            new_lst.append(num)
+    
+    return max(new_lst)
+
+print(find_largest_even([2,2,1,8,3,5]))
 
 
 
