45ca9592bfc59296f454caacd5ff0ae30b62c3e4
DeniseYT <denise.ytlin@gmail.com>
2022-01-26 03:54:56 +0000

    Initial commit

--- decode.py
+++ decode.py
@@ -1,19 +1,36 @@
-In this challenge, you’ll write a decoder.
+# In this challenge, you’ll write a decoder.
 
-A valid code is a sequence of numbers and letters, always starting with a number and ending with letter(s).
+# A valid code is a sequence of numbers and letters, always starting with a number and ending with letter(s).
 
-Each number tells you how many characters to skip before finding a good letter. 
-After each good letter should come the next next number.
+# Each number tells you how many characters to skip before finding a good letter. 
+# After each good letter should come the next next number.
 
-For example, the string “hey” could be encoded by “0h1ae2bcy”. 
-This means “skip 0, find the ‘h’, skip 1, find the ‘e’, skip 2, find the ‘y’”.
+# For example, the string “hey” could be encoded by “0h1ae2bcy”. 
+# This means “skip 0, find the ‘h’, skip 1, find the ‘e’, skip 2, find the ‘y’”.
 
-A single letter should work:
->>> decode("0h")
-'h'
->>> decode("2abh")
-'h'
+# A single letter should work:
+# >>> decode("0h")
+# 'h'
+# >>> decode("2abh")
+# 'h'
 
-Longer patterns should work:
->>> decode("0h1ae2bcy")
-'hey'
+# Longer patterns should work:
+# >>> decode("0h1ae2bcy")
+# 'hey'
+
+
+# My solution
+
+def decode(s):
+    """Decode a string."""
+
+    
+        
+
+
+    
+
+    
+
+    
+print(decode("2abh3abha"))
