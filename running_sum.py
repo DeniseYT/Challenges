ab5afe360e39cf74a7004f1b59e2a153213b6cff
DeniseYT <denise.ytlin@gmail.com>
2022-02-07 22:19:25 +0000

    Complete

--- running_sum.py
+++ running_sum.py
@@ -1,21 +1,36 @@
-https://leetcode.com/problems/running-sum-of-1d-array/
+# https://leetcode.com/problems/running-sum-of-1d-array/
 
-1480. Running Sum of 1d Array
+# 1480. Running Sum of 1d Array
 
-Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
-Return the running sum of nums.
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+# Return the running sum of nums.
 
-Example 1:
-Input: nums = [1,2,3,4]
-Output: [1,3,6,10]
-Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+# Example 1:
+# Input: nums = [1,2,3,4]
+# Output: [1,3,6,10]
+# Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
 
-Example 2:
-Input: nums = [1,1,1,1,1]
-Output: [1,2,3,4,5]
-Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+# Example 2:
+# Input: nums = [1,1,1,1,1]
+# Output: [1,2,3,4,5]
+# Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
 
-Example 3:
-Input: nums = [3,1,2,10,1]
-Output: [3,4,6,16,17]
+# Example 3:
+# Input: nums = [3,1,2,10,1]
+# Output: [3,4,6,16,17]
 
+
+# My solution
+def running_sum(lst):
+
+    new_list = []
+    count = 0
+
+    for i in range(len(lst)):
+
+        count += lst[i]
+        new_list.append(count)
+    
+    return new_list
+        
+print(running_sum([1,2,3,4]))
